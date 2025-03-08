774514fcefc467dd6b2184a7620242ff5dee9052
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-03-08 12:35:21 -0300

    att

--- teste.c
+++ teste.c
@@ -1,2 +1,18 @@
 
 
+
+#if !defined(DTW_1_0_0_MOCK_STDIO_H) || !defined(DTW_1_0_0_MOCK_ALL) 
+    #include <stdio.h>
+#endif
+
+typedef struct dtw_1_0_0_Dependencies{
+    int (*print)(const char *fmt,...);
+}dtw_1_0_0_Dependencies;
+
+
+
+
+int main(){
+  
+
+}
